df9d0d9acfc7d3fad000162794fda56632f1ee76
Wallace Silva Brito <101600525+Wall90s@users.noreply.github.com>
2023-11-27 22:54:25 -0300

    Adicionando Tela inicial do projeto Procedural

---
+++ procedural/main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args) {
+        telaInicial();
+    }
+    
+    public static void telaInicial(){
+        System.out.println("- Jogo do Velho -");
+        System.out.println("");
+        System.out.println("escolha uma opção");
+        System.out.println("1 - Jogar");
+        System.out.println("2 - Como jogar");
+        System.out.println("3 - Créditos");
+        System.out.println("");
+        System.out.println("feito com <3 por Wall90s");
+        System.out.println("2023");
+    }
+}
